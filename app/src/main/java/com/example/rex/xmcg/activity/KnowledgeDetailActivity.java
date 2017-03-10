23d7bb6ffee4ca1a07b919153838e68eff006f32
moqiang02 <moqiang02@163.com>
2017-03-10 17:31:15 +0800

    445

---
+++ app/src/main/java/com/example/rex/xmcg/activity/KnowledgeDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.rex.xmcg.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rex.xmcg.R;
+
+public class KnowledgeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knowledge_detail);
+    }
+}
